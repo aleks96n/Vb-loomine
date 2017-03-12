aa479a86fd44b5e19c69462f0a51f1b3456640dd
BigDaddy <aleksnik1996@gmail.com>
2017-03-12 21:23:47 +0200

    Test1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
-  def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_path
-  end
+#  def create
+#    user = User.from_omniauth(env["omniauth.auth"])
+#    session[:user_id] = user.id
+#    redirect_to root_path
+#  end
 
   def destroy
     session[:user_id] = nil
