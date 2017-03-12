b1fd784e64aec2dc763d950470811692fe0edd50
BigDaddy <aleksnik1996@gmail.com>
2017-03-12 15:30:14 +0200

    multiface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper :all
+  helper_method :current_user
+  I18n.config.available_locales = [:en, :wk]
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  before_filter :set_user_language
+
+  private
+  def set_user_language
+    I18n.locale = 'wk'
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,3 @@
 class WelcomeController < ApplicationController
+  protect_from_forgery with: :exception
 end
