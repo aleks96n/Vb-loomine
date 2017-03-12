1379126d4fa8e2127d497897a8f8b2bc42908078
Aleksis Zalitis <aleksis.zalitis@gmail.com>
2017-03-12 15:59:04 +0200

    Now possible to log in as well, although stil needs to add lots of things, added header and footer file as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
+  get '/about' => 'welcome#about'
   get '/signup' => "users#new"
   get '/login' => "sessions#new"
   post '/login' => "sessions#create"
