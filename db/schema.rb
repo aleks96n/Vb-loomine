835acb31d21be1a1924cf8d6ec7bd5291b8da4f5
Aleksis Zalitis <aleksis.zalitis@gmail.com>
2017-03-15 01:51:05 +0200

    Fix to how db is created, hopefully should work heroku now, fixing the sign up bug

--- db/schema.rb
+++ db/schema.rb
@@ -16,16 +16,6 @@ ActiveRecord::Schema.define(version: 20170312214435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "userq", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "oauth_token"
-    t.datetime "oauth_expires_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
