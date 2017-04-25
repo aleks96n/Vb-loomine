d8fed0c9ad08134edf92a841c205557c3b09e1cb
Aleksis Zalitis <aleksis.zalitis@gmail.com>
2017-04-26 02:25:48 +0300

    small changes to profile update and user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+
   #before_filter :set_user_language
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,14 +21,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-    def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-        redirect_to @user
-    else
-        # if update is unsuccessful
-    end
-end
+  def update
+     @user = User.find(params[:id])
+     if @user.update_attributes(user_params)
+       # Handle a successful update.
+     else
+       render 'edit'
+     end
+   end
 
 
   private
