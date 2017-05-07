8f8af58465ed41a40bd2730ffc26e0c83834b2a9
Aleksis Zalitis <aleksis.zalitis@gmail.com>
2017-05-07 16:59:51 +0300

    small fix for book migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312214435) do
+ActiveRecord::Schema.define(version: 20170425211256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string   "authod"
+    t.integer  "price"
+    t.boolean  "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "book_id"
+    t.string   "user"
+    t.integer  "rating"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reviews", ["book_id"], name: "index_reviews_on_book_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -31,4 +50,5 @@ ActiveRecord::Schema.define(version: 20170312214435) do
     t.string   "profile_image"
   end
 
+  add_foreign_key "reviews", "books"
 end
